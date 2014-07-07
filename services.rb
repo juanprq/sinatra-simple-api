660fe41e331a535f231c5d5ff04873d87da69dde
Juan Pablo Ramírez <jota@gyffu.com>
2014-07-07 17:04:00 -0500

    Avances en la implementación de los servicios

--- services.rb
+++ services.rb
@@ -27,11 +27,23 @@ end
 
 # Servicio para la creación del recurso usuario.
 post '/users' do
-  
+  # Se verifica el identificador que no esté repetido.
+  id = params[:id]
+  if @@params[id] == null
+    # Se emplea el método auxiliar para filtrar los parámetros que vienen de la petición para asignarlos a una nueva
+    # variable.
+    user = accept_params(params, :name, :last_name, :document)
+
+    @@users[id] = user
+    headers["location"] = "/users/#{id}"
+    status 201
+  else
+    # Se envía un código de error al cliente para indicarle que algo asalió mal.
+    halt 409
 end
 
 # Servicio para obtener un usuario en específico.
-get '/users/{id}' do |id|
+get '/users/:id' do |id|
   # Se obtiene el usuario de la colección.
   user = @@users[id]
 
@@ -42,4 +54,25 @@ get '/users/{id}' do |id|
   else
     # En caso de no encontrarse en la colección se responde con el código 404 indicando que el recurso no existe.
     halt 404
+end
+
+# Servicio para actualizar un recurso.
+put '/users/:id' do |id|
+    if @@params[id] != nil
+    # Se pasan los parámetros a la variable usuario
+    user = accept_params(params, :name, :last_name, :document)
+
+    # Se actualiza el valor del usuario en la colección de objetos.
+    @@users[id] = user
+
+    # Se responde el código 204 para indicar que fué exitosa la operación pero no hay un contenido para responder.
+    status 204
+  else
+    # Se responde con el código de error ya que el recurso no fué encontrado.
+    halt 404
+end
+
+# Servicio para eliminar un recurso.
+delete '/users/:id' do |id|
+  
 end
