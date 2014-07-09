7d0089bbf0e6ce250ba44052a0d5ae82eac4015b
Juan Pablo Ramírez <jota@gyffu.com>
2014-07-09 14:50:52 -0500

    Se agrega validación para que el identificador sea solo dígitos

--- services.rb
+++ services.rb
@@ -31,6 +31,11 @@ helpers do
     result
   end
 
+  # Valida que el valor del identificador sean dígitos.
+  def valid_id?(id)
+    id && id.to_s =~ /^\d+$/
+  end
+
 end
 
 # Servicio para obtener la colección de usuarios del sistema.
@@ -46,37 +51,46 @@ post '/users' do
   data = JSON.parse(request.body.string)
   logger.info "Se va a crear un nuevo usuario con los datos: #{data.to_json}"
 
-  # Se verifica el identificador que no esté repetido.
-  id = data['id'].to_i
+  if(valid_id?(data['id'])) 
+    # Se verifica el identificador que no esté repetido.
+    id = data['id'].to_i
 
-  if settings.users[id].nil?
-    # Se asignan los valores al objeto de usuario.
-    user = accept_params(data['user'], 'name', 'last_name', 'document')
+    if settings.users[id].nil?
+      # Se asignan los valores al objeto de usuario.
+      user = accept_params(data['user'], 'name', 'last_name', 'document')
 
-    settings.users[id] = user
-    headers["Location"] = "/users/#{id}"
-    status 201
+      settings.users[id] = user
+      headers["Location"] = "/users/#{id}"
+      status 201
+    else
+      logger.error 'Ocurrió un error al tratar de crear un nuevo usuario'
+      # Se envía un código de error al cliente para indicarle que algo asalió mal.
+      status 409
+    end
   else
-    logger.error 'Ocurrió un error al tratar de crear un nuevo usuario'
-    # Se envía un código de error al cliente para indicarle que algo asalió mal.
-    status 409
+    status 404
   end
 end
 
 # Servicio para obtener un usuario en específico.
 get '/users/:id' do |id|
   logger.info 'Se va a obtener el usuario con id: #{id}'
-  # Se obtiene el usuario de la colección.
-  user = settings.users[id.to_i]
 
-  # Se verifica si el usuario si es retornado.
-  if user.nil?
-    logger.info "El usuario con id: #{id} no fué encontrado en el sistema"
-    # En caso de no encontrarse en la colección se responde con el código 404 indicando que el recurso no existe.
-    status 404
+  if valid_id?(id)
+    # Se obtiene el usuario de la colección.
+    user = settings.users[id.to_i]
+
+    # Se verifica si el usuario si es retornado.
+    if user.nil?
+      logger.info "El usuario con id: #{id} no fué encontrado en el sistema"
+      # En caso de no encontrarse en la colección se responde con el código 404 indicando que el recurso no existe.
+      status 404
+    else
+      # Se retorna el objeto encontrado.
+      user.to_json
+    end
   else
-    # Se retorna el objeto encontrado.
-    user.to_json
+    status 404
   end
 end
 
@@ -87,43 +101,50 @@ put '/users/:id' do |id|
 
   logger.info "Se va a actualizar el usuario con id: #{id} con los datos: #{data}"
 
-  # Se castea a entero.
-  id = id.to_i
+  if valid_id?(id)
+    # Se castea a entero.
+    id = id.to_i
+    if settings.users[id].nil?
+      logger.info "El usuario con id #{id} no fué encontrado"
+      # Se responde con el código de error ya que el recurso no fué encontrado.
+      status 404
+    else
+      # Se pasan los parámetros a la variable usuario
+      user = accept_params(data, 'name', 'last_name', 'document')
 
-  if settings.users[id].nil?
-    logger.info "El usuario con id #{id} no fué encontrado"
-    # Se responde con el código de error ya que el recurso no fué encontrado.
-    status 404
+      # Se actualiza el valor del usuario en la colección de objetos.
+      settings.users[id] = user
+
+      # Se responde el código 204 para indicar que fué exitosa la operación pero no hay un contenido para responder.
+      status 204
+    end
   else
-    # Se pasan los parámetros a la variable usuario
-    user = accept_params(data, 'name', 'last_name', 'document')
-
-    # Se actualiza el valor del usuario en la colección de objetos.
-    settings.users[id] = user
-
-    # Se responde el código 204 para indicar que fué exitosa la operación pero no hay un contenido para responder.
-    status 204
+    status 404
   end
 end
 
 # Servicio para eliminar un recurso.
 delete '/users/:id' do |id|
   logger.info "Se va a eliminar el usuario con id: #{id}"
-  # Se castea a entero
-  id = id.to_i
 
-  # Se verifica la existencia del recurso en el sistema.
-  if settings.users[id].nil?
-    logger.info "El usuario con id: #{id} no fué encontrado"
-    # En caso de no encontrarlo se retorna el código de error.
-    status 404    
+  if valid_id?(id)
+    # Se castea a entero
+    id = id.to_i
+    # Se verifica la existencia del recurso en el sistema.
+    if settings.users[id].nil?
+      logger.info "El usuario con id: #{id} no fué encontrado"
+      # En caso de no encontrarlo se retorna el código de error.
+      status 404    
+    else
+      # Se remueve el recurso de la colección
+      user = settings.users.delete(id)
+
+      # Se retorna un código de éxito y el contenido del usuario.
+      status 200
+      user
+    end
   else
-    # Se remueve el recurso de la colección
-    user = settings.users.delete(id)
-
-    # Se retorna un código de éxito y el contenido del usuario.
-    status 200
-    user
+    status 404
   end
 end
 
